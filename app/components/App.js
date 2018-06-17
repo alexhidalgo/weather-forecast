82d64a5fee939408555f202a1bace47465d18bd9
alexhidalgo <aj1hidalgo@gmail.com>
2018-06-16 22:54:07 -0500

    Add base components App, Nav and Search. Created css file.

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Nav from './Nav'
+import Search from './Search'
 
 export default function App () {
   return (
     <div>
-    app rendered
+      <Nav />
+      <Search />
     </div>
   )
 }
---
+++ app/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Nav (props) {
+  return (
+    <div className='nav-bar'>
+      <p>
+        Weather App
+      </p>
+      <div>
+        <input />
+        <button>
+          Get Weather
+        </button>
+      </div>
+    </div>
+  )
+}
